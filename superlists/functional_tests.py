fc2c364b9421b26513afb4c399990f0d9a51dd12
shwetakh <shwetakh@sdp08.mh.lucent.com>
2015-06-20 14:26:54 -0400

    Changed FT to use python unittest

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
+import unittest
 
-browser=webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTestCase(unittest.TestCase):
+	def setUp(self):
+		self.browser=webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+	def tearDown(self):
+		self.browser.quit()
+	def test_can_make_list_and_retrieve_later(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('To-Do',self.browser.title)
+		self.fail('Finish writig TestCase')
 
-assert 'Django' in browser.title 
+if __name__=='__main__':
+	unittest.main()	
