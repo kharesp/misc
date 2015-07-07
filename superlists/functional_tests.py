9439011c3228b506f1896d4940c2b9d56f573099
kharesp <shweta.p.khare@gmail.com>
2015-07-07 07:03:46 -0400

    Extended functional test. Refactored home page view test to use a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys 
 
 class NewVisitorTestCase(unittest.TestCase):
 	def setUp(self):
@@ -8,9 +9,31 @@ class NewVisitorTestCase(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 	def test_can_make_list_and_retrieve_later(self):
+		#check if browser title has To-Do
 		self.browser.get('http://localhost:8000')
 		self.assertIn('To-Do',self.browser.title)
-		self.fail('Finish writig TestCase')
+		
+		#check if header h1 has To-Do
+		header_text=self.browser.find_element_by_tag_name('h1').text 
+		self.assertIn('To-Do',header_text)
 
+		#check if there is an input box with default text: Enter a To-Do item
+		input_box=self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(input_box.get_attribute('placeholder'),
+			'Enter a To-Do item')
+
+		#enter first To-Do item and press ENTER
+		input_box.send_keys("Item 1")
+		input_box.send_keys(Keys.ENTER) 
+
+		#check if To-Do item got added to the table of all To-Do items 
+		table=self.browser.find_element_by_id('id_list_table')
+		rows=table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text=="Item 1" for row in rows))
+		
+		#check if there is another input box for adding more To-Do items 
+		self.fail('Finish the test')
+		
 if __name__=='__main__':
 	unittest.main()	
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.core.urlresolvers import resolve 
 from lists.views import home_page 
@@ -12,8 +13,10 @@ class HomePageTest(TestCase):
 	def test_home_page_view_returns_html(self):
 		request=HttpRequest()
 		response=home_page(request)
-		self.assertTrue(response.
-			content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>',response.content)
-		self.assertTrue(response.
-			content.endswith(b'</html>'))
+		'''
+		check if returned html file is expected home.html
+		decode() converts response.content bytes to unicode string
+		'''
+		self.assertEqual(response.content.decode(),
+			render_to_string('home.html'))
+
